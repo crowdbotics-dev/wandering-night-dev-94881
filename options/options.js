9b5fd6e1229e91a24f8b35949d95467848b77198
Crowdbotics <engineering@crowdbotics.com>
2023-06-26 11:26:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-night-dev-94881",
+  url: "https://wandering-night-dev-94881.botics.co",
+  api: "https://wandering-night-dev-94881.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
